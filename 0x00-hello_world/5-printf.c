2ae4a84fdc391a17fa67ebcd93fcb624a0d51f69
root <diribabaru430@gmail.com>
2023-03-17 03:52:57 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main -  Hello ,printf
+*
+* return: good 0 (GREAT)
+*/
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a pieco of art,\n");
+	return (0);
+}
+
